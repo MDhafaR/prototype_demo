9755ba0d7040451e227c1052d4f27ca17562dd13
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2024-09-18 09:58:19 +0700

    make polyline with click marker

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,9 +8,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late MapController _mapController;
   Position? _currentPosition;
   bool _isLoading = false;
-  late MapController _mapController;
+  List<LatLng> _routePoints = [];
 
   Future<void> _getCurrentPosition() async {
     setState(() {
@@ -28,13 +29,21 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         _currentPosition = position;
       });
-
     } catch (e) {
       debugPrint(e.toString());
     } finally {
       setState(() {
         _isLoading = false;
       });
+    }
+  }
+
+  void _centerOnCurrentLocation() {
+    if (_currentPosition != null) {
+      _mapController.move(
+        LatLng(_currentPosition!.latitude, _currentPosition!.longitude),
+        16,
+      );
     }
   }
 
@@ -54,51 +63,93 @@ class _HomePageState extends State<HomePage> {
       body: Stack(
         children: [
           FlutterMap(
-            mapController: _mapController,
-            options: MapOptions(
-                  initialCenter: const LatLng(-6.9481298, 107.6595105),
-                  initialZoom: 15,
+              mapController: _mapController,
+              options: MapOptions(
+                initialCenter: const LatLng(-6.9481298, 107.6595105),
+                initialZoom: 15,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.technoinfinity.prototype',
                 ),
-            children: [
-            TileLayer(
-                    urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    userAgentPackageName: 'com.technoinfinity.prototype',
+                MarkerLayer(markers: [
+                  Marker(
+                    rotate: true,
+                    height: 50,
+                    width: 50,
+                    point: LatLng(-6.9481298, 107.6595104),
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _routePoints = [
+                            LatLng(-6.9481298, 107.6595104),
+                            LatLng(_currentPosition!.latitude,
+                                _currentPosition!.longitude)
+                          ];
+                        });
+                      },
+                      icon: Icon(Icons.location_on, size: 50),
+                    ),
                   ),
-            MarkerLayer(markers: [
-              Marker(
+                  Marker(
                       rotate: true,
                       height: 50,
                       width: 50,
-                      point: LatLng(-6.9481298, 107.6595104),
-                      child: Icon(Icons.location_on, size: 50)
+                      point: LatLng(-6.9539400, 107.6599104),
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _routePoints = [
+                              LatLng(-6.9539400, 107.6599104),
+                              LatLng(_currentPosition!.latitude,
+                                  _currentPosition!.longitude)
+                            ];
+                          });
+                        },
+                        icon: Icon(Icons.location_on, size: 50),
+                      )),
+                  Marker(
+                    rotate: true,
+                    height: 50,
+                    width: 50,
+                    point: LatLng(-6.9481298, 107.6634104),
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _routePoints = [
+                            LatLng(-6.9481298, 107.6634104),
+                            LatLng(_currentPosition!.latitude,
+                                _currentPosition!.longitude)
+                          ];
+                        });
+                      },
+                      icon: Icon(Icons.location_on, size: 50),
                     ),
-                    Marker(
-                        rotate: true,
-                        height: 50,
-                        width: 50,
-                        point: LatLng(-6.9539400, 107.6599104),
-                        child: Icon(Icons.location_on, size: 50)),
+                  ),
+                  if (_currentPosition != null) ...[
                     Marker(
                       rotate: true,
-                      height: 50,
-                      width: 50,
-                      point: LatLng(-6.9481298, 107.6634104),
-                      child: Icon(Icons.location_on, size: 50)
+                      height: 40,
+                      width: 40,
+                      point: LatLng(_currentPosition!.latitude,
+                          _currentPosition!.longitude),
+                      child:
+                          Icon(Icons.my_location, size: 50, color: Colors.blue),
                     ),
-                    if (_currentPosition != null) ...[
-                      Marker(
-                        rotate: true,
-                        height: 40,
-                        width: 40,
-                        point: LatLng(_currentPosition!.latitude,
-                            _currentPosition!.longitude),
-                        child:
-                            Icon(Icons.my_location, size: 50, color: Colors.blue),
-                      ),
-                    ],
-            ])
-          ]
-          ),
+                  ],
+                ]),
+                PolylineLayer(
+                  polylines: [
+                    Polyline(
+                      points: _routePoints,
+                      color: Colors
+                          .green, // Asumsi ColorsLib.hijau adalah warna hijau
+                      strokeWidth: 5,
+                    ),
+                  ],
+                ),
+              ]),
           if (_isLoading) Center(child: CircularProgressIndicator()),
         ],
       ),
--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -4,6 +4,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:local_auth/local_auth.dart';
+import 'package:open_route_service/open_route_service.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 part 'home_page.dart';
