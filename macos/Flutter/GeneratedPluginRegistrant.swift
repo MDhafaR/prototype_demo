3c6f7240a25acb2a9df5325fc601c3a895cc1d08
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2024-09-18 09:35:27 +0700

    dependencies for get my location

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import geolocator_apple
+import local_auth_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
 }
