560fdc23aed2ec3bc88734ecb02161be50d88024
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2024-09-21 10:58:30 +0700

    new init

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,17 @@
+part of 'pages.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My App"),
+      ),
+      body: Center(
+        child: Text("Hello, World!"),
+      ),
+    );
+  }
+}
